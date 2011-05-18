0608b2f3e212adbc6ae3a23d2b32ee285d169140
Jon Kunkee <jonathan.kunkee@gmail.com>
2011-05-18 14:49:59 -0600

    var declaration convention observed

--- say.js
+++ say.js
@@ -2,11 +2,13 @@
 (function () {
 	"use strict";
 
-	var hello = require("./build/default/hello");
+	//require the module like any other module
+	var hello = require("./build/default/hello")
+	//The first argument is at array index 2.
+	  , n = 2;
 	
 	console.log("The .exports from the module you wrote:");
 	console.log(hello);
-	var n = 2;
 	console.log("Your input: " + process.argv[n]);
 	console.log("Your function's result: " + hello.func(process.argv[n]));
 
